66b3352e8b929594cb3b8f0ab55b89101d4fdb11
huseynalipanahov <mi7US8AXF@gmail.com>
2023-11-26 20:24:22 +0400

    add category deal section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.scss';
-
+import Super_Deal from './Components/Super_Deal';
+import Best_Category from './Components/Best_Category';
 function App() {
   return (
     <>
+    <Super_Deal/>
+    <Best_Category/>
     </>
   );
 }
