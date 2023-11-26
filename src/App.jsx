5991d3bb9e336cd0f358356d6dd4c9fd12a6427f
unknown <mi7pvtuj0@code.edu.az>
2023-11-26 19:59:47 +0400

    add section blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.scss';
+import Blog from './components/Blog';
+import BlueAdverst from './components/BlueAdverst';
+import Copyright from './components/Copyright';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+    <Blog/>
+    <BlueAdverst/>
+    <Footer/>
+    <Copyright/>
     </>
   );
 }
